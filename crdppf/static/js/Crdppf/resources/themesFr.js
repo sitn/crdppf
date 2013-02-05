5c69fe93ef1a6d8d6b972d1d69f877f9c73cab49
Olivier Monod <olivier.monod@ne.ch>
2013-02-05 08:13:53 +0000

    Added development json for layer tree

--- crdppf/static/js/Crdppf/resources/themesFr.js
+++ crdppf/static/js/Crdppf/resources/themesFr.js
@@ -1,24 +1,39 @@
-//definition of layers associated to themes
+﻿//definition of layers associated to themes
 
 Ext.namespace('Crdppf');
 
-Crdppf.layerListFr = { '73': {'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '79': {'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '87': {'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '88': {'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '96': {'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '97': {'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '103':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '104':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '108':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '116':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '117':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '118':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '119':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '131':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '132':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '145':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '74-NE':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '159':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'},
-                '50-NE':{'adress_l1':'Adresse 1', 'adress_l2':'Adresse 2', 'adress_l3':'Adresse 3'}
-                };
+Crdppf.layerListFr = {'0': {'at14_zones_communales':'Zones communales','at08_zones_cantonales':'Zones cantonales','at39_itineraires_pedestres':'Intinéraires pédestres','clo_couloirs':'Couloirs d\'obstacles aériens','clo_cotes_altitude_surfaces':'Côtes d\'altitude des surfaces','en07_canepo_accidents':'Sites polluées : accidents','en07_canepo_decharges':'Sites pollués : décharges','en07_canepo_decharges_points':'Sites pollués : décharges (points)','en07_canepo_decharges_polygones':'Sites pollués : décharges (polygones)', 'en07_canepo_entreprises':'Sites pollués : entreprises', 'en07_canepo_entreprises_points':'Sites pollués : entreprises (points)', 'en07_canepo_entreprises_polygones':'Sites pollués : entreprises (polygones)'},  
+                        '73': {'at14_zones_communales':'Zones communales','at08_zones_cantonales':'Zones cantonales'},
+                        '79': {'at39_itineraires_pedestres':'Intinéraires pédestres'},
+                        '87': {},
+                        '88': {},
+                        '96': {},
+                        '97': {},
+                        '103':{},
+                        '104':{},
+                        '108':{'clo_couloirs':'Couloirs d\'obstacles aériens','clo_cotes_altitude_surfaces':'Cotes d\'altitude des surfaces'},
+                        '116':{'en07_canepo_accidents':'Sites polluées : accidents','en07_canepo_decharges':'Sites pollués : décharges','en07_canepo_decharges_points':'Sites pollués : décharges (points)','en07_canepo_decharges_polygones':'Sites pollués : décharges (polygones)', 'en07_canepo_entreprises':'Sites pollués : entreprises', 'en07_canepo_entreprises_points':'Sites pollués : entreprises (points)', 'en07_canepo_entreprises_polygones':'Sites pollués : entreprises (polygones)'},
+                        '117':{},
+                        '118':{},
+                        '119':{},
+                        '131':{},
+                        '132':{},
+                        '145':{},
+                        '74-NE':{},
+                        '159':{},
+                        '50-NE':{}
+                        };
+                        
+Crdppf.layerTreeListFr = [{'text':'Chemin pour piétons','id':'79', 'leaf':false,'checked':false, children: [{ 'text':'Itinéraires pédestres','id':'at39_itineraires_pedestres','leaf':true, 'checked':false}]},
+                          {'text':'Aménagement', 'id':'73', 'leaf':false,'checked':false, children: 
+                              [{'text':'Zones communales','id':'at14_zones_communales','leaf':true,'checked':false},
+                               {'text':'Zones cantonales', 'id':'at08_zones_cantonales', 'leaf': true,'checked':false}]},
+                          {'text':'Cadastre des sites pollués', 'id':'116', 'leaf':false,'checked':false, children: 
+                              [{'text':'Accidents','id':'en07_canepo_accidents','leaf':true,'checked':false},
+                               {'text':'Sites pollués : décharges', 'id':'en07_canepo_decharges', 'leaf': true,'checked':false},
+                               {'text':'Sites pollués : décharges (points)', 'id':'en07_canepo_decharges_points', 'leaf': true,'checked':false},
+                               {'text':'Sites pollués : décharges (polygones)', 'id':'en07_canepo_decharges_polygones', 'leaf': true,'checked':false},
+                               {'text':'Sites pollués : entreprises', 'id':'en07_canepo_entreprises', 'leaf': true,'checked':false},
+                               {'text':'Sites pollués : entreprises (points)', 'id':'en07_canepo_entreprises_points', 'leaf': true,'checked':false},
+                               {'text':'Sites pollués : entreprises (polygones)', 'id':'en07_canepo_entreprises_polygones', 'leaf': true,'checked':false}]}
+                        ];
