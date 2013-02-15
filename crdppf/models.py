08d7a0491283143060d62a4d1514472406742764
Francois Voisard <francois.voisard@ne.ch>
2013-02-15 13:13:30 +0000

    Added new models such as topics and authority

--- crdppf/models.py
+++ crdppf/models.py
@@ -1,27 +1,59 @@
 from sqlalchemy import (
-    Column,
-    Integer,
+    Column, 
+    Sequence, 
+    Integer, 
     Text,
+    String, 
+    Boolean, 
+    ForeignKey, 
+    Float
     )
+    
+from sqlalchemy.orm import relationship, backref
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
 import sqlahelper
 
-Base = sqlahelper.get_base()
 DBSession = sqlahelper.get_session()
 
 from papyrus.geo_interface import GeoInterface
-from geoalchemy import GeometryColumn, Geometry, Polygon, GeometryDDL
+from geoalchemy import (
+    GeometryColumn, 
+    Geometry, 
+    Polygon
+    )
 
-class Prod(Base):
-    __tablename__ = 'product'
-    __table_args__ = {'schema': 'public', 'autoload': True}
-    id_product = Column('id_product', Integer, primary_key=True)
+Base = sqlahelper.get_base()
 
-class Commune(GeoInterface, Base):
-    __tablename__ = 'communes'
+class Topics(Base):
+    __tablename__ = 'topics'
+    __table_args__ = {'schema': 'crdppf', 'autoload': True}
+    
+class Authority(Base):
+    __tablename__ = 'authority'
+    __table_args__ = {'schema': 'crdppf', 'autoload': True}
+    
+class Cadastre(Base):
+    __tablename__ = 'la02_cadastres'
+    __table_args__ = {'schema': 'general', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+
+class ImmeublesCanton(Base):
+    __tablename__ = 'immeubles_canton'
     __table_args__ = {'schema': 'public', 'autoload': True}
-    numcom = Column('numcom', Integer, primary_key=True)
+    noobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+
+class NomLocalLieuDit(Base):
+    __tablename__ = 'nom_local_lieu_dit'
+    __table_args__ = {'schema': 'public', 'autoload': True}
+    idcnlo = Column(String(30), primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class Zoneprotection(Base):
+    __tablename__ = 'en01_zone_sect_protection_eaux'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(String(40), primary_key=True)
     geom = GeometryColumn(Geometry(srid=21781))
-
