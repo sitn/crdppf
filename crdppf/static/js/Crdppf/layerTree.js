3dbfce8e4b249b71ffaaf4ba9848513f84269efd
Olivier Monod <olivier.monod@ne.ch>
2013-02-20 15:19:19 +0000

    New file containing a custom layerTree Object

---
+++ crdppf/static/js/Crdppf/layerTree.js
@@ -0,0 +1,78 @@
+﻿Ext.namespace('Crdppf');
+
+// create layer tree and append nodes & subnodes to it
+Crdppf.LayerTree = function createLayerTree () {
+    // create layer tree object
+    var overlaysList = [];
+    layerTree = new Ext.tree.TreePanel({
+        title: 'LayerTree',
+        height: 300,
+        width: 400,
+        useArrows:true,
+        autoScroll:true,
+        animate:true,
+        lines: true,
+        enableDD:false,
+        containerScroll: false,
+        rootVisible: false,
+        frame: true,
+        id: 'layerTree'
+    });
+    // define root node
+    rootLayerTree = new Ext.tree.TreeNode({
+        text: 'rootLayerTree',
+        draggable:false,
+        id:'rootLayerTree'})
+    ll = Crdppf.layerListFr.themes;
+    // iterate over themes and create nodes
+    for (i=0;i<ll.length;i++){
+        var themeId = ll[i].id;
+        var themeNode =  new Ext.tree.TreeNode({
+            text: ll[i].name,
+            draggable:false,
+            id: ll[i].id,
+            leaf: false,
+            checked:false,
+            listeners: {
+                'checkchange': function(node, checked){
+                    if(checked){
+                        for (k=0; k < node.childNodes.length; k++){
+                            node.childNodes[k].getUI().toggleCheck(true);
+                        }
+                    }else{ 
+                        for (k=0; k < node.childNodes.length; k++){
+                            node.childNodes[k].getUI().toggleCheck(false);
+                        }
+                }
+                }
+            }
+        })
+        // iterate over layers and create subnodes
+        for (var keys in ll[i].layers)
+        {
+            var lName = ll[i].layers[keys];
+            layerNode =  new Ext.tree.TreeNode({
+                text: ll[i].layers[keys],
+                draggable: false,
+                id: keys,
+                leaf: true,
+                checked:false,
+                listeners: {
+                        'checkchange': function(node, checked){
+                            if(checked){
+                                overlaysList.push(node.id)
+                                MapO.setOverlays(overlaysList)
+                            }else{
+                                overlaysList.remove(node.id)
+                                MapO.setOverlays(overlaysList)
+                            }
+                        }
+                    }
+                })
+            themeNode.appendChild(layerNode)
+        }
+        rootLayerTree.appendChild(themeNode)
+    }
+    layerTree.setRootNode(rootLayerTree);
+    return layerTree
+}
