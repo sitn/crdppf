b87ba220047ed7b0d6a234e2cf13c9382c057588
Olivier Monod <olivier.monod@ne.ch>
2013-02-21 07:51:25 +0000

    Limited the feature returned to 1 single feature

--- crdppf/ogcproxy.py
+++ crdppf/ogcproxy.py
@@ -33,7 +33,7 @@ def ogcproxy(request):
     resp, content = http.request(url, method=method, body=body, headers=h)
 
     if method == "POST" and is_get_feature(body):
-        content = limit_featurecollection(content, limit=200)
+        content = limit_featurecollection(content, limit=1)
 
     headers = {"Content-Type": resp["content-type"]}
     
