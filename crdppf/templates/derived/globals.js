bbdfbbbe9b221c3f116ddc725d2aa30233c2be99
Olivier Monod <olivier.monod@ne.ch>
2013-02-22 11:03:39 +0000

    Added getFeature url global variable Crdppf.getFeatureUrl

--- crdppf/templates/derived/globals.js
+++ crdppf/templates/derived/globals.js
@@ -2,5 +2,6 @@ if(!window.Crdppf) Crdppf = {};
 
 Crdppf.baseUrl = "${request.route_url('home')}";
 Crdppf.wmsUrl = "${request.registry.settings['crdppf_wms']}";
+Crdppf.getFeatureUrl = "${request.registry.settings['getFeatureUrl']}";
 Crdppf.ogcproxyUrl = "${request.route_url('ogcproxy')}";
 
