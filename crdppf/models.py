57eb92127bc85828d5cda926e10a7e71990c18b8
Olivier Monod <olivier.monod@ne.ch>
2013-02-22 07:52:00 +0000

    Added models for get features queries

--- crdppf/models.py
+++ crdppf/models.py
@@ -26,6 +26,8 @@ from geoalchemy import (
 
 Base = sqlahelper.get_base()
 
+# START models used for static extraction and general models
+
 class Topics(Base):
     __tablename__ = 'topic'
     __table_args__ = {'schema': 'crdppf', 'autoload': True}
@@ -57,3 +59,89 @@ class Zoneprotection(Base):
     __table_args__ = {'schema': 'environnement', 'autoload': True}
     idobj = Column(String(40), primary_key=True)
     geom = GeometryColumn(Geometry(srid=21781))
+    
+# STOP models used for static extraction and general models
+
+# START models used for GetFeature queries
+    
+# models for theme: pedestrian ways
+
+class PedestrianWays(Base):
+    __tablename__ = 'at39_itineraires_pedestres'
+    __table_args__ = {'schema': 'amenagement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+
+# models for theme: allocation plan
+
+class CommunalArea(Base):
+    __tablename__ = 'at14_zones_communales'
+    __table_args__ = {'schema': 'amenagement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class StateArea(Base):
+    __tablename__ = 'at08_zones_cantonales'
+    __table_args__ = {'schema': 'amenagement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+
+# models for obstacles to navigation
+
+class Corridors(Base):
+    __tablename__ = 'clo_couloirs'
+    __table_args__ = {'schema': 'amenagement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class AltitudeRatings(Base):
+    __tablename__ = 'clo_cotes_altitude_surfaces'
+    __table_args__ = {'schema': 'amenagement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+
+# models for theme: register of polluted sites
+
+class PollutedSitesAccidents(Base):
+    __tablename__ = 'en07_canepo_accidents'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class PollutedSitesLandDumps(Base):
+    __tablename__ = 'en07_canepo_decharges'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class PollutedSitesLandDumpsPts(Base):
+    __tablename__ = 'en07_canepo_decharges_points'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class PollutedSitesLandDumpsPoly(Base):
+    __tablename__ = 'en07_canepo_decharges_polygones'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+
+class PollutedSitesCompanies(Base):
+    __tablename__ = 'en07_canepo_entreprises'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class PollutedSitesCompaniesPts(Base):
+    __tablename__ = 'en07_canepo_entreprises_points'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+class PollutedSitesCompaniesPoly(Base):
+    __tablename__ = 'en07_canepo_entreprises_polygones'
+    __table_args__ = {'schema': 'environnement', 'autoload': True}
+    idobj = Column(Integer, primary_key=True)
+    geom =GeometryColumn(Geometry(2,srid=21781))
+    
+# STOP models used for GetFeature queries
