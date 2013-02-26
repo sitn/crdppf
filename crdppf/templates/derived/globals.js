df104e5a557a42284c884a3e084423a5df989ac0
Olivier Monod <olivier.monod@ne.ch>
2013-02-26 17:00:30 +0000

    Added variable to images directory

--- crdppf/templates/derived/globals.js
+++ crdppf/templates/derived/globals.js
@@ -1,6 +1,7 @@
 if(!window.Crdppf) Crdppf = {};
 
 Crdppf.baseUrl = "${request.route_url('home')}";
+Crdppf.imagesDir = "${request.route_url('images')}";
 Crdppf.wmsUrl = "${request.registry.settings['crdppf_wms']}";
 Crdppf.getFeatureUrl = "${request.registry.settings['getFeatureUrl']}";
 Crdppf.ogcproxyUrl = "${request.route_url('ogcproxy')}";
