a95be18ed28012b1c891b9f40be5ef058d48b65f
Olivier Monod <olivier.monod@ne.ch>
2013-02-26 08:01:23 +0000

    Removed commented content

--- crdppf/static/js/Crdppf/resources/themesFr.js
+++ crdppf/static/js/Crdppf/resources/themesFr.js
@@ -2,29 +2,6 @@
 
 Ext.namespace('Crdppf');
 
-// Crdppf.layerListFrOld = [{  '0': {'at14_zones_communales':'Zones communales','at08_zones_cantonales':'Zones cantonales','at39_itineraires_pedestres':'Intinéraires pédestres','clo_couloirs':'Couloirs d\'obstacles aériens','clo_cotes_altitude_surfaces':'Côtes d\'altitude des surfaces','en07_canepo_accidents':'Sites polluées : accidents','en07_canepo_decharges':'Sites pollués : décharges','en07_canepo_decharges_points':'Sites pollués : décharges (points)','en07_canepo_decharges_polygones':'Sites pollués : décharges (polygones)', 'en07_canepo_entreprises':'Sites pollués : entreprises', 'en07_canepo_entreprises_points':'Sites pollués : entreprises (points)', 'en07_canepo_entreprises_polygones':'Sites pollués : entreprises (polygones)'},  
-                        // '73': {'at14_zones_communales':'Zones communales','at08_zones_cantonales':'Zones cantonales'},
-                        // '79': {'at39_itineraires_pedestres':'Intinéraires pédestres'},
-                        // '87': {},
-                        // '88': {},
-                        // '96': {},
-                        // '97': {},
-                        // '103':{},
-                        // '104':{},
-                        // '108':{'clo_couloirs':'Couloirs d\'obstacles aériens','clo_cotes_altitude_surfaces':'Cotes d\'altitude des surfaces'},
-                        // '116':{'en07_canepo_accidents':'Sites polluées : accidents','en07_canepo_decharges':'Sites pollués : décharges','en07_canepo_decharges_points':'Sites pollués : décharges (points)','en07_canepo_decharges_polygones':'Sites pollués : décharges (polygones)', 'en07_canepo_entreprises':'Sites pollués : entreprises', 'en07_canepo_entreprises_points':'Sites pollués : entreprises (points)', 'en07_canepo_entreprises_polygones':'Sites pollués : entreprises (polygones)'},
-                        // '117':{},
-                        // '118':{},
-                        // '119':{},
-                        // '131':{},
-                        // '132':{},
-                        // '145':{},
-                        // '74-NE':{},
-                        // '159':{},
-                        // '50-NE':{}
-                        // }];
-// 'at14_zones_communales':'Zones communales','at08_zones_cantonales':'Zones cantonales',
-
 Crdppf.layerListFr = {
     "type": "ThemesCollection", 
         'themes' :[     {'id':'0','name':'Itinéraires pédestres', 'layers':{'at39_itineraires_pedestres':'Intinéraires pédestres'}},
