4849caf81481aaf51f80a15dfbbd77ca64e09d6e
Olivier Monod <olivier.monod@ne.ch>
2013-02-28 10:31:31 +0000

    corrected get_feature url

--- crdppf/templates/derived/globals.js
+++ crdppf/templates/derived/globals.js
@@ -3,6 +3,6 @@ if(!window.Crdppf) Crdppf = {};
 Crdppf.baseUrl = "${request.route_url('home')}";
 Crdppf.imagesDir = "${request.route_url('images')}";
 Crdppf.wmsUrl = "${request.registry.settings['crdppf_wms']}";
-Crdppf.getFeatureUrl = "${request.registry.settings['getFeatureUrl']}";
+Crdppf.getFeatureUrl = "${request.route_url('get_features')}";
 Crdppf.ogcproxyUrl = "${request.route_url('ogcproxy')}";
 
