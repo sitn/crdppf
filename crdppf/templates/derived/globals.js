399d1d70a1e9a98ceae64d9e9ee99552220d079d
Olivier Monod <olivier.monod@ne.ch>
2013-03-05 06:40:15 +0000

    Added globals for the set_language and get_language URL's

--- crdppf/templates/derived/globals.js
+++ crdppf/templates/derived/globals.js
@@ -4,5 +4,7 @@ Crdppf.baseUrl = "${request.route_url('home')}";
 Crdppf.imagesDir = "${request.route_url('images')}";
 Crdppf.wmsUrl = "${request.registry.settings['crdppf_wms']}";
 Crdppf.getFeatureUrl = "${request.route_url('get_features')}";
+Crdppf.setLanguageUrl = "${request.route_url('set_language')}";
+Crdppf.getLanguageUrl = "${request.route_url('get_language')}";
 Crdppf.ogcproxyUrl = "${request.route_url('ogcproxy')}";
 
