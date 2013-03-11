e3bac3c6e5b8064a9d3ef64343f281ac1e577f74
Olivier Monod <olivier.monod@ne.ch>
2013-03-11 10:54:35 +0000

    Created new languages & parametrization files

--- crdppf/static/js/Crdppf/resources/De.js
+++ crdppf/static/js/Crdppf/resources/De.js
@@ -1,16 +1,16 @@
-Ext.namespace('Crdppf');
+ÔªøExt.namespace('Crdppf');
 
 // Themes structure and content definition
 Crdppf.layerListDe = {
     "type": "ThemesCollection",
         'themes' :[  
                         {'id':'73','image':'amenagement.gif','name':'Raumplanung', 'layers':{'at14_zones_communales':'Gemeindes Zone','at08_zones_cantonales':'Kantonalen Zone','at39_itineraires_pedestres':'Fusswege'}},
-                        {'id':'108','image':'aeroports.gif','name':'Flughafen', 'layers':{'clo_couloirs':'Couloirs d\'obstacles aÈriens','clo_cotes_altitude_surfaces':'Cotes d\'altitude des surfaces'}},
-                        {'id':'116','image':'sites_pollues.gif','name':'Belastete Standorte','layers':{'en07_canepo_accidents':'Belastete Standorte: Unf‰lle','en07_canepo_decharges':'Belastete Standorte: Deponien','en07_canepo_decharges_points':'Belastete Standorte: Deponien (punkte)','en07_canepo_decharges_polygones':'Belastete Standorte: Deponien (polygon)', 'en07_canepo_entreprises':'Belastete Standorte: Deponien', 'en07_canepo_entreprises_points':'Belastete Standorte: Unternehmen (Punkte)', 'en07_canepo_entreprises_polygones':'Belastete Standorte: Unternehmen (Polygon)'}},
+                        {'id':'108','image':'aeroports.gif','name':'Flughafen', 'layers':{'clo_couloirs':'Couloirs d\'obstacles a√©riens','clo_cotes_altitude_surfaces':'Cotes d\'altitude des surfaces'}},
+                        {'id':'116','image':'sites_pollues.gif','name':'Belastete Standorte','layers':{'en07_canepo_accidents':'Belastete Standorte: Unf√§lle','en07_canepo_decharges':'Belastete Standorte: Deponien','en07_canepo_decharges_points':'Belastete Standorte: Deponien (punkte)','en07_canepo_decharges_polygones':'Belastete Standorte: Deponien (polygon)', 'en07_canepo_entreprises':'Belastete Standorte: Deponien', 'en07_canepo_entreprises_points':'Belastete Standorte: Unternehmen (Punkte)', 'en07_canepo_entreprises_polygones':'Belastete Standorte: Unternehmen (Polygon)'}},
                         {'id':'999','image':'foret.gif','name':'Wald','layers':{'at39_itineraires_pedestres':'Fusswege'}},
                         {'id':'998','image':'routes_nationales.gif','name':'Nationalstrassen','layers':{'at39_itineraires_pedestres':'Fusswege'}},                        
                         {'id':'997','image':'protection_eaux.gif','name':'Grundwasserschutz','layers':{'at39_itineraires_pedestres':'Fusswege'}},
-                        {'id':'996','image':'bruit.gif','name':'L‰rm','layers':{'at39_itineraires_pedestres':'Fusswege'}}
+                        {'id':'996','image':'bruit.gif','name':'L√§rm','layers':{'at39_itineraires_pedestres':'Fusswege'}}
         ]
     };
     
@@ -22,17 +22,17 @@ Crdppf.labelsDe  ={
     'mapContainerTab':'Karte',
     'legalBasisTab':'Gesetzliche Grundlagen',
     'layerTreeTitle':'Daten',
-    'selectAllLayerLabel':'Alle Ebenen ausw‰hlen',
+    'selectAllLayerLabel':'Alle Ebenen ausw√§hlen',
     'lawTabLabel':'Rechtsvorschriften',
     'additionnalInfoTab':'Verweise und Zusatzinformationen',
     'infoTabLabel':'Informationen',
     'legendPanelTitle':'Legende',
     'searchBoxEmptyTxt':'Suchen...',
     'olCoordinates':'Koordinaten',
-    'restrictionPanelTitle':'Eigentumsbeschr‰nkungen',
-    'restrictionPanelTxt':'Eigentumsbeschr‰nkungen der Parzelle Nr. ',
+    'restrictionPanelTitle':'Eigentumsbeschr√§nkungen',
+    'restrictionPanelTxt':'Eigentumsbeschr√§nkungen der Parzelle Nr. ',
     'noActiveLayertxt':'Keine Ebene aktiv',
-    'restrictionFoundTxt':'Eigentumsbeschr‰nkung Nr. ',
-    'disclaimerTxt':'Haftungsausschluss : Der Kanton Neuenburg ¸bernimmt keine Garantie f¸r die Exaktheit und Vollst‰ndigkeit der Gesetzestexte in ihrer elektronischen Form. Diese Dokumente ...',
+    'restrictionFoundTxt':'Eigentumsbeschr√§nkung Nr. ',
+    'disclaimerTxt':'Haftungsausschluss : Der Kanton Neuenburg √ºbernimmt keine Garantie f√ºr die Exaktheit und Vollst√§ndigkeit der Gesetzestexte in ihrer elektronischen Form. Diese Dokumente ...',
     'mapBottomTxt':'<b>Daten sind nicht rechtsverbindlich, <a style="color:#660000;" href="http://sitn.ne.ch/web/conditions_utilisation/contrat_SITN_MO.htm" target="_new">&copy; SITN</a></b>'
 };
