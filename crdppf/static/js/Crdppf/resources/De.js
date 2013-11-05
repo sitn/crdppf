de0d93f763d54b897d8dac79542fdf9920a56116
Francois Voisard <francois.voisard@ne.ch>
2013-11-05 23:05:35 +0100

    translations fixed

--- crdppf/static/js/Crdppf/resources/De.js
+++ crdppf/static/js/Crdppf/resources/De.js
@@ -4,22 +4,22 @@ Ext.namespace('Crdppf');
 Crdppf.layerListDe = {
     "type": "ThemesCollection",
         'themes' :[  
-                        {'id':'73','image':'amenagement.gif','name':'Raumplanung', 'layers':{'at14_zones_communales':'Gemeindes Zone','at08_zones_cantonales':'Kantonalen Zone','at39_itineraires_pedestres':'Fusswege','at28_limites_constructions':'Limites constructions'}},
+                        {'id':'73','image':'amenagement.gif','name':'Raumplanung', 'layers':{'at14_zones_communales':'Kommunale Raumplanung','at08_zones_cantonales':'Kantonale Raumplanung','at28_limites_constructions':'Bauabstandslinien'}},
                         {'id':'998','image':'routes_nationales.gif','name':'Nationalstrassen','layers':{'at39_itineraires_pedestres':'Fusswege'}},                        
                         {'id':'500','image':'chemin_fer.gif','name':'Eisenbahnen','layers':{'at39_itineraires_pedestres':'Fusswege'}},                        
-                        {'id':'108','image':'aeroports.gif','name':'Flughäfen', 'layers':{'clo_couloirs':'Couloirs d\'obstacles aériens','clo_cotes_altitude_surfaces':'Cotes d\'altitude des surfaces'}},
+                        {'id':'108','image':'aeroports.gif','name':'Flughäfen', 'layers':{'clo_couloirs':'Höhenbegrenzungskataster','clo_cotes_altitude_surfaces':'Höhenbegrenzungskataster- Höhen'}},
                         {'id':'116','image':'sites_pollues.gif','name':'Belastete Standorte','layers':{'en07_canepo_accidents':'Belastete Standorte: Unfälle','en07_canepo_decharges':'Belastete Standorte: Deponien','en07_canepo_decharges_points':'Belastete Standorte: Deponien (punkte)','en07_canepo_decharges_polygones':'Belastete Standorte: Deponien (polygon)', 'en07_canepo_entreprises':'Belastete Standorte: Deponien', 'en07_canepo_entreprises_points':'Belastete Standorte: Unternehmen (Punkte)', 'en07_canepo_entreprises_polygones':'Belastete Standorte: Unternehmen (Polygon)'}},
-                        {'id':'997','image':'protection_eaux.gif','name':'Grundwasserschutz','layers':{'en01_zone_sect_protection_eaux':'Grundwasserschutz'}},
-                        {'id':'996','image':'bruit.gif','name':'Lärm','layers':{'en05_degres_sensibilite_bruit':'Lärm'}},
+                        {'id':'997','image':'protection_eaux.gif','name':'Grundwasserschutz','layers':{'en01_zone_sect_protection_eaux':'Grundwasserschutzzonen und -sektoren'}},
+                        {'id':'996','image':'bruit.gif','name':'Lärm','layers':{'en05_degres_sensibilite_bruit':'Lärmepfindlichkeitsstufen'}},
                         {'id':'999','image':'foret.gif','name':'Wald','layers':{'r157_lim_foret':'Waldgrenzen'}}
         ]
     };
     
 Crdppf.baseLayersDe = { 
         'baseLayers':[
-            {'id': '1', 'image': 'plan_cadastral.png', 'name': 'Plan cadastral', 'wmtsname': 'plan_cadastral_c2c'},
+            {'id': '1', 'image': 'plan_cadastral.png', 'name': 'Katasterplan', 'wmtsname': 'plan_cadastral_c2c'},
             {'id': '2', 'image': 'plan_cadastral.png', 'name': 'Orthophoto', 'wmtsname': 'ortho2011'},
-            {'id': '3', 'image': 'plan_cadastral.png', 'name': 'Plan de ville', 'wmtsname': 'plan_ville_c2c'}
+            {'id': '3', 'image': 'plan_cadastral.png', 'name': 'Ortsplan', 'wmtsname': 'plan_ville_c2c'}
         ]
 };
     
@@ -46,9 +46,9 @@ Crdppf.labelsDe  ={
     'restrictionFoundTxt':'Eigentumsbeschränkung Nr. ',
     'disclaimerTxt':'Haftungsausschluss : Der Kanton Neuenburg übernimmt keine Garantie für die Exaktheit und Vollständigkeit der Gesetzestexte in ihrer elektronischen Form. Diese Dokumente ...',
     'mapBottomTxt':'<b>Daten sind nicht rechtsverbindlich, <a style="color:#660000;" href="http://sitn.ne.ch/web/conditions_utilisation/contrat_SITN_MO.htm" target="_new">&copy; SITN</a></b>',
-    'intersectToolTipMessage':'Räumliche Beziehung: Schnittpunkt',
+    'intersectToolTipMessage':'Räumliche Beziehung: geschnitten',
     'withinToolTipMessage':'Räumliche Beziehung: innerhalb',
-    'adjacentToolTipMessage':'Räumliche Beziehungen: benachbarte',
+    'adjacentToolTipMessage':'Räumliche Beziehungen: benachbart',
     'printPdfWindowsTitle': 'PDF',
     'noSelectedParcelMessage': 'Wählen Sie zuerst ein Grundstück !',
     'printButtonTlp':'Auszug erstellen',
@@ -61,7 +61,7 @@ Crdppf.labelsDe  ={
     'chooseExtractMsg': 'Welchen Auszugstyp möchten Sie ?',
     'chooseExtractMsgTitle':'Auszugstyp Wahl',
     'reducedExtract': 'Reduzierter Auszug',
-    'extendedExtract': 'Konpletter Auszug',
+    'extendedExtract': 'Kompletter Auszug',
     'cancelExtract': 'Abbrechen',
     'generateExtract': 'Auszug erstellen'
 
