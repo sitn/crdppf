54525bcf07cac12c6939f4014f71fbf9f840a827
monodo <monodo@WSSGRF030.ne.ch>
2014-01-21 14:05:29 +0100

    Take get_feature_function out to a separate file, idem from table2model matching table

---
+++ crdppf/util/get_feature_functions.py
@@ -0,0 +1,69 @@
+# -*- coding: UTF-8 -*-
+from simplejson import loads as sloads 
+from crdppf.models import *
+import csv
+from sqlalchemy import or_
+from papyrus.geojsonencoder import dumps
+import math
+from crdppf.util.table2model_match import table2model
+
+def get_features_function(parcelGeom, params):
+
+    # split the layer list string into proper python list
+    csvReader = csv.reader([params['layerList']], skipinitialspace=True)
+
+    # iterate over layer and make intersects queries
+    itemList = []
+    for item in csvReader:
+        itemList.append(item)
+    layerList = itemList[0]
+
+    test = 'empty'
+    # retrieve models from table2model
+    for layer in layerList:
+        model = table2model[layer]
+
+    # spatial analysis
+    featureList = []
+    for layer in layerList:
+        targetModel = table2model[layer]
+        intersectResult = DBSession.query(targetModel).filter(or_(targetModel.geom.intersects(parcelGeom), targetModel.geom.within(parcelGeom))).all()
+        if intersectResult:
+            # create geojson output with custom attributes
+            for feature in intersectResult:
+                geometryType = DBSession.scalar(feature.geom.geometry_type())
+                geomType = ''
+                intersectionMeasure = -9999
+                intersectionMeasureTxt = ''
+                if geometryType == 'ST_Polygon' or geometryType == 'ST_MultiPolygon':
+                    intersectionMeasure = DBSession.scalar(feature.geom.intersection(parcelGeom).area())
+                    if intersectionMeasure >= 1:
+                        intersectionMeasureTxt = ' : ' + str(math.ceil(intersectionMeasure*10)/10) + ' [m2]'
+                        geomType = 'Polygone'
+                        jsonFeature = sloads(dumps(feature))
+                        jsonFeature['properties']['layerName'] = layer
+                        jsonFeature['properties']['intersectionMeasure'] = intersectionMeasureTxt
+                        jsonFeature['properties']['geomType'] = 'area'
+                        featureList.append(jsonFeature)
+
+                elif geometryType == 'ST_Line' or geometryType == 'ST_MultiLineString' or geometryType == 'ST_LineString':
+                    intersectionMeasure = intersectionMeasure = DBSession.scalar(feature.geom.intersection(parcelGeom).length())
+                    if intersectionMeasure >= 1:
+                        intersectionMeasureTxt = ' '
+                        geomType = 'Ligne'
+                        jsonFeature = sloads(dumps(feature))
+                        jsonFeature['properties']['layerName'] = layer
+                        jsonFeature['properties']['intersectionMeasure'] = intersectionMeasureTxt
+                        jsonFeature['properties']['geomType'] = 'line'
+                        featureList.append(jsonFeature)
+                elif geometryType == 'ST_Point' or geometryType == 'ST_MultiPoint':
+                    featureMeasure = -9999
+                    geomType = 'Point'
+                    intersectionMeasureTxt = ' ' #' : point'
+                    jsonFeature = sloads(dumps(feature))
+                    jsonFeature['properties']['layerName'] = layer
+                    jsonFeature['properties']['intersectionMeasure'] = intersectionMeasureTxt
+                    jsonFeature['properties']['geomType'] = 'point'
+                    featureList.append(jsonFeature)
+
+    return featureList
---
+++ crdppf/util/table2model_match.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+from crdppf.models import *
+# Matching dictionnary model-table name
+table2model = {
+    'r73_affectations_primaires': PrimaryLandUseZones,
+    'r73_zones_superposees': SecondaryLandUseZones,
+    'r73_perimetres_superposes': ComplementaryLandUsePerimeters,
+    'r73_contenus_lineaires': LandUseLinearConstraints,
+    'r73_contenus_ponctuels': LandUsePointConstraints,
+    'r103_bazl_projektierungszonen_flughafenanlagen': CHAirportProjectZones,
+    'r108_bazl_sicherheitszonenplan': CHAirportSecurityZones,
+    'r116_sites_pollues': PollutedSites,
+    'r119_bav_belastete_standorte_oev': CHPollutedSitesPublicTransports,
+    'r131_zone_prot_eau': Zoneprotection,
+    'r132_perimetre_prot_eau': WaterProtectionPerimeters,
+    'r145_sens_bruit': RoadNoise,
+    'r157_lim_foret': ForestLimits,
+    'r159_dist_foret': ForestDistances
+}
