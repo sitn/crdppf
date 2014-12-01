ece70350e24d00e9269de1b7ee0979aba72fd78d
Francois Voisard <francois.voisard@ne.ch>
2014-12-01 13:40:45 +0100

    some cleanup of unnecessary stuff

--- crdppfportal/models.py
+++ crdppfportal/models.py
@@ -1,34 +1,18 @@
+# -*- coding: utf-8 -*-
+
 from sqlalchemy import(
     Column, 
     Sequence, 
     Integer, 
     Text,
-    String, 
-    Boolean, 
-    ForeignKey, 
+    String,
     Float)
     
 from sqlalchemy.orm import relationship, backref, deferred
-
 from zope.sqlalchemy import ZopeTransactionExtension
-
 import sqlahelper
 
-DBSession = sqlahelper.get_session()
-
-from papyrus.geo_interface import GeoInterface
-from geoalchemy import (
-    GeometryColumn, 
-    Geometry, 
-    Polygon,
-    WKTSpatialElement,
-    GeometryDDL#,
-#    WKBSpatialElement
-    )
-
 from crdppf import db_config
-
-srid_ = db_config['srid']
 
 Base = sqlahelper.get_base()
 
